87560e0343f3b14e296c788c098d74cfa44c75e5
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-08 15:27:29 +0100

    custom colors added in tailwind.config file

--- devjobs-web-app/tailwind.config.js
+++ devjobs-web-app/tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      primary: '#9e7f66',
+      secondary: '#939BF4',
+      darkBlue: '#19202D',
+      midnight: '#121721',
+      white: '#FFFFFF',
+      lightGrey: '#F4F6F8',
+      gray: '#9DAEC2',
+      darkGrey: '#6E8098'
+    },
     extend: {},
   },
   plugins: [],
