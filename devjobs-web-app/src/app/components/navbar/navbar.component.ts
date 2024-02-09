876accf786ee41bfbc5ee41281a2c077c4c718fd
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-09 14:09:51 +0100

    navigation bar added with responsiveness
    The navbar contains a logo which links to the home page of the web app , a dark mode switcher and a localStorage function that stores the current theme in memory

--- devjobs-web-app/src/app/components/navbar/navbar.component.ts
+++ devjobs-web-app/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isDark = false;
 
+  ngOnInit(): void {
+    const storedTheme = localStorage.getItem('theme');
+    console.log(storedTheme)
+    this.isDark = storedTheme === 'dark';
+    document.body.setAttribute('data-theme', this.isDark ? 'dark' : 'light');
+  }
+
+  themeSwitcher() {
+    this.isDark = !this.isDark;
+    document.body.setAttribute('data-theme', this.isDark ? 'dark' : 'light');
+    localStorage.setItem('theme', this.isDark ? 'dark' : 'light');
+  }
+  
 }
