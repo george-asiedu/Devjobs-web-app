83d172a60686c2f7f0cf87906c8d80731754d06b
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-10 15:25:36 +0100

    filter component dark mode update

--- devjobs-web-app/src/app/components/filter/filter.component.ts
+++ devjobs-web-app/src/app/components/filter/filter.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+  isCheck =false 
 
+  onChangeIsCheck() {
+    this.isCheck=!this.isCheck
+    
+  }
 }
