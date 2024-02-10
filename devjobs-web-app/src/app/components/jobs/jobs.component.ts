103181a769a7e26d865d86279e182ef89faa2a9d
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-10 22:15:13 +0100

    jobs data populated
    Flex cards created to display the jobs available, location, company, company logo, job title and more

--- devjobs-web-app/src/app/components/jobs/jobs.component.ts
+++ devjobs-web-app/src/app/components/jobs/jobs.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Jobs } from '../../model/jobs';
 import { AllJobsService } from '../../service/all-jobs.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-jobs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.css'
 })
