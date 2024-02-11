6261d4ebd433e03f09b7e45fb0464975c14295b3
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-11 10:50:38 +0100

    job details service injected

--- devjobs-web-app/src/app/pages/job-details/job-details.component.ts
+++ devjobs-web-app/src/app/pages/job-details/job-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Jobs } from '../../model/jobs';
+import { AllJobsService } from '../../service/all-jobs.service';
 
 @Component({
   selector: 'app-job-details',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './job-details.component.html',
   styleUrl: './job-details.component.css'
 })
-export class JobDetailsComponent {
+export class JobDetailsComponent implements OnInit {
+  jobDetails: Jobs = {} as Jobs;
 
+  constructor( private allJobService: AllJobsService) {}
+
+  ngOnInit(): void {
+    const jobId = '1'
+    this.allJobService.getJobDetails(jobId)
+      .subscribe(
+        (jobDetail) => {
+          console.log(jobDetail);
+          
+          this.jobDetails = jobDetail
+        }
+      )
+  }
 }
