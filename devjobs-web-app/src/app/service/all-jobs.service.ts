aace56b9837ffcd27dd270a993ce59baa5a7d611
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-11 09:56:15 +0100

    error handling logic implemented in the service
    1. I imported HttpErrorResponse to handle specific details from the HTTP error.
    2. I  used the catchError operator to intercept errors on the observable returned by getJobs
    3. Inside handleError, I checked if the error is a client-side or network error or a backend error based on the error.error instanceof ErrorEvent condition.
    4. log the error message for both cases.
    5.We return an observable with a user-facing error message using throwError

--- devjobs-web-app/src/app/service/all-jobs.service.ts
+++ devjobs-web-app/src/app/service/all-jobs.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { Jobs } from '../model/jobs';
 
 @Injectable({
@@ -14,5 +14,22 @@ export class AllJobsService {
 
   getJobs(): Observable<Jobs[]> {
     return this.http.get<Jobs[]>(this.devjobsUrl)
+      .pipe(
+        catchError(this.handleError)
+      )
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    if(error.error instanceof ErrorEvent) {
+      console.error('An error occurred: ', error.error.message)
+    } else {
+      console.error(
+        `Status code ${error.status} ` + 
+        `body was: ${error.error}` 
+      )
+    }
+    return throwError(()=> 
+      'Something went wrong while fetching jobs data. Please try again later.'
+      )
   }
 }
