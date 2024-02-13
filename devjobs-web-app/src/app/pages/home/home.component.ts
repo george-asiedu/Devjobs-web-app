4f2277efe82e7d83cb1d4835209cead4a494a66b
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-13 14:32:57 +0100

    jobs data fetched in the home component

--- devjobs-web-app/src/app/pages/home/home.component.ts
+++ devjobs-web-app/src/app/pages/home/home.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FilterComponent } from '../../components/filter/filter.component';
 import { JobsComponent } from '../../components/jobs/jobs.component';
+import { Jobs } from '../../model/jobs';
+import { AllJobsService } from '../../service/all-jobs.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FilterComponent, JobsComponent],
+  imports: [FilterComponent, JobsComponent, CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  jobs: Jobs[] = []
+  loading: boolean = true
+  error: string = ''
+  filteredJobs: Jobs[] = [];
 
+  constructor (private allJobsService: AllJobsService) {}
+
+  ngOnInit(): void {
+    this.allJobsService.getJobs().subscribe(
+      (job) => {
+        this.jobs = job
+        this.loading = false
+        this.filteredJobs = job
+      }
+    ), 
+    ((err: string) => {
+      this.error = err
+      this.loading = false
+    })
+  }
 }
