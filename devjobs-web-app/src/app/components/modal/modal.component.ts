758723a11939b18649ff3bcdd93bf5a05a5be6b6
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-14 10:38:06 +0100

    modal component

---
+++ devjobs-web-app/src/app/components/modal/modal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+
+}
